25c0a6ccbb8c4babc8bab40c737f586cea9b2234
edward sharp <edward@edwardsharp.net>
2019-03-30 23:36:27 -0400

    pretty-ify

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var SCROLL_OFFSET = document.querySelector('.index.sticky-nav') ? -118 : -58;
 
-  document.querySelectorAll('.index.sticky-nav').forEach(function(el){
+  document.querySelectorAll('.index.sticky-nav').forEach(function (el) {
     // ScrollSpy has trouble determining the scroll offset for sticky elements that have already been scrolled past because they stick to the top: so their scrollTop is 0 instead of where the elem is actually in the window. so, here, split those elements apart to make it work, more-better. 
     var indexAnchor = document.createElement(el.nodeName);
     indexAnchor.classList.add('index');
@@ -11,40 +11,40 @@ document.addEventListener('DOMContentLoaded', function() {
     el.removeAttribute('id');
     el.parentNode.insertBefore(indexAnchor, el);
   });
-  
+
   var tocHeader = document.getElementById('toc-header');
-  if(tocHeader && PAGE_TOC){
-    var navIndexHeading = document.createElement("li"); 
+  if (tocHeader && PAGE_TOC) {
+    var navIndexHeading = document.createElement("li");
     navIndexHeading.classList.add('nav-index-heading');
-    var navIndexHeadingContent = document.createTextNode(PAGE_TOC); 
+    var navIndexHeadingContent = document.createTextNode(PAGE_TOC);
     var navIndexBackToTop = document.createElement('span')
     navIndexBackToTop.innerHTML = '<a class="btn-floating btn-small btn-flat waves-effect waves-light nav-back-to-top tooltipped" data-tooltip="Back to top"><i class="material-icons">arrow_upward</i></a>';
     navIndexHeading.appendChild(navIndexHeadingContent);
     navIndexHeading.appendChild(navIndexBackToTop);
     tocHeader.appendChild(navIndexHeading);
-    document.querySelector('.nav-back-to-top').addEventListener('click', function(e){
-      document.querySelector('#slide-out').scrollTo(0,0);
+    document.querySelector('.nav-back-to-top').addEventListener('click', function (e) {
+      document.querySelector('#slide-out').scrollTo(0, 0);
     });
   }
-  
+
   var items = [];
-  document.querySelectorAll('.index').forEach(function(el){
-    items.push('<li><a href="#'+el.id+'"'+' class="waves-effect sidenav-close">'+el.id+'</a></li>');
+  document.querySelectorAll('.index').forEach(function (el) {
+    items.push('<li><a href="#' + el.id + '"' + ' class="waves-effect sidenav-close">' + el.id + '</a></li>');
   });
 
   var navUl = document.querySelector('#scrollspy-nav ul');
-  if(navUl){
+  if (navUl) {
     navUl.innerHTML = items.join('');
     navUl.classList.remove('hidden');
   }
 
-  var scrollSpyEnter = function(navElemSelector, id){
+  var scrollSpyEnter = function (navElemSelector, id) {
     var scrollspyNavElem = document.querySelector('#scrollspy-nav');
     var navElem = scrollspyNavElem.querySelector(navElemSelector);
-    scrollspyNavElem.querySelectorAll('.active').forEach(function(el){
+    scrollspyNavElem.querySelectorAll('.active').forEach(function (el) {
       el.classList.remove('active');
     })
-    if(navElem){
+    if (navElem) {
       navElem.classList.add('active');
       document.querySelector('#slide-out').scrollTo(window.scrollX, navElem.offsetTop - scrollspyNavElem.offsetTop)
     }
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var debounceScrollSpyEnter = debounce(scrollSpyEnter, 100);
   M.ScrollSpy.init(document.querySelectorAll('.index'), {
     scrollOffset: SCROLL_OFFSET,
-    getActiveElement: function(id){
+    getActiveElement: function (id) {
       var navElemSelector = 'a[href="#' + id + '"]';
       debounceScrollSpyEnter(navElemSelector, id);
       return navElemSelector;
@@ -61,51 +61,51 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   // odang! try to create hyperlinkz for "see also"
-  try{
-    var see = document.evaluate("//em[contains(., 'see')]", document, null, XPathResult.ANY_TYPE, null );
+  try {
+    var see = document.evaluate("//em[contains(., 'see')]", document, null, XPathResult.ANY_TYPE, null);
     var seez = see.iterateNext();
     var tagzToUpdate = [];
-    while(seez){
+    while (seez) {
       var txt = seez.nextSibling.textContent;
       var tags = [];
       // split(/,|;/) 
-      if(/;/.test(txt)){
+      if (/;/.test(txt)) {
         tags = txt.split(';');
-      }else{
+      } else {
         tags = txt.split(',');
       }
-      for(var i=0; i<tags.length; i++){
+      for (var i = 0; i < tags.length; i++) {
         var tag = tags[i].trim();
-        if(tag !== ''){
-          var found = document.evaluate("//p[starts-with(., '"+tag+" ')]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null ).snapshotLength;
-          if(found > 0){
-            var tagLinkz = document.evaluate("//p[starts-with(., '"+tag+" ')]", document, null, XPathResult.ANY_TYPE, null );
+        if (tag !== '') {
+          var found = document.evaluate("//p[starts-with(., '" + tag + " ')]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength;
+          if (found > 0) {
+            var tagLinkz = document.evaluate("//p[starts-with(., '" + tag + " ')]", document, null, XPathResult.ANY_TYPE, null);
             var tagLink = tagLinkz.iterateNext();
-            if(tagLink){
-              tagzToUpdate.push({tag: tag, parentEl: seez.parentElement});
+            if (tagLink) {
+              tagzToUpdate.push({ tag: tag, parentEl: seez.parentElement });
               var parentEl = seez.parentElement;
-              parentEl && tagzToUpdate.push({tag: tag, parentEl: seez.parentElement, offsetTop: tagLink.offsetTop});
+              parentEl && tagzToUpdate.push({ tag: tag, parentEl: seez.parentElement, offsetTop: tagLink.offsetTop });
             }
           }
         }
       } // end for loop
-      seez=see.iterateNext();
+      seez = see.iterateNext();
     } // end while
     // need to defer dom mutationz until after iterating through all the see XPath itemz...
-    for(var i=0; i<tagzToUpdate.length; i++){
+    for (var i = 0; i < tagzToUpdate.length; i++) {
       var tag = tagzToUpdate[i].tag;
       var parentEl = tagzToUpdate[i].parentEl;
       var offsetTop = tagzToUpdate[i].offsetTop;
-      parentEl.innerHTML = parentEl.innerHTML.replace(tag, '<a class="see-scroll" data-scroll="'+offsetTop+'">'+tag+'</a>');
+      parentEl.innerHTML = parentEl.innerHTML.replace(tag, '<a class="see-scroll" data-scroll="' + offsetTop + '">' + tag + '</a>');
     }
 
-    var seeScrollClick = function(e){
+    var seeScrollClick = function (e) {
       var offsetTop = e.target.getAttribute('data-scroll');
       !isNaN(parseInt(offsetTop)) && window.scrollTo(window.scrollX, parseInt(offsetTop) + SCROLL_OFFSET);
     }
-    document.querySelectorAll('.see-scroll').forEach(function(el){
+    document.querySelectorAll('.see-scroll').forEach(function (el) {
       el.addEventListener('click', seeScrollClick);
     });
-  }catch(e){ /* eh... */ }
+  } catch (e) { /* eh... */ }
 
 });
