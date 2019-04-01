66f2beafc5a2419359e699a7bf3502e1417d91a2
edward sharp <edward@edwardsharp.net>
2019-03-31 20:58:49 -0400

    see also linkz on terms indices (#38)
    * add XPath voodoo to create see also hyperlinkz
    
    * pretty-ify
    
    * fix blooper :feelsgood:
    
    * add titlez & tooltipz to index project linkz…
    
    * update 2014 contributorz submodule
    
    * add indexes to volume’s TOC
    
    * fix some see also linking bugz
    
    * bugfix: add boundry to match whole word
    
    * fix up see also linking :feelsgood:
    
    * update 2011 terms index
    
    * sub module updatez
    
    * sub module updatez
    
    * tidy’n
    
    * sub module updatez
    
    * link to project in volume

--- _includes/project_links.md
+++ _includes/project_links.md
@@ -1,8 +1,8 @@
 
 {% for project in include.projects %}
-[{{project.pages}}]: {{project.url}}
+[{{project.pages}}]: {{project.url}} "{{project.title}} -- {{project.contributor}}"
 {%- assign pages = project.pages | split: '-' %}
 {% for page in pages %}
-[{{page}}]: {{project.url}}
+[{{page}}]: {{project.url}} "{{project.title}} -- {{project.contributor}}"
 {% endfor %}
 {% endfor %}
--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,50 +1,50 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var SCROLL_OFFSET = document.querySelector('.index.sticky-nav') ? -118 : -58;
 
-  document.querySelectorAll('.index.sticky-nav').forEach(function(el){
+  document.querySelectorAll('.index.sticky-nav').forEach(function (el) {
     // ScrollSpy has trouble determining the scroll offset for sticky elements that have already been scrolled past because they stick to the top: so their scrollTop is 0 instead of where the elem is actually in the window. so, here, split those elements apart to make it work, more-better. 
     var indexAnchor = document.createElement(el.nodeName);
     indexAnchor.classList.add('index');
     indexAnchor.setAttribute('id', el.getAttribute('id'));
     indexAnchor.innerHTML = '&nbsp;';
     el.classList.remove('index');
-    el.setAttribute('id', undefined);
+    el.removeAttribute('id');
     el.parentNode.insertBefore(indexAnchor, el);
   });
-  
+
   var tocHeader = document.getElementById('toc-header');
-  if(tocHeader && PAGE_TOC){
-    var navIndexHeading = document.createElement("li"); 
+  if (tocHeader && PAGE_TOC) {
+    var navIndexHeading = document.createElement("li");
     navIndexHeading.classList.add('nav-index-heading');
-    var navIndexHeadingContent = document.createTextNode(PAGE_TOC); 
+    var navIndexHeadingContent = document.createTextNode(PAGE_TOC);
     var navIndexBackToTop = document.createElement('span')
     navIndexBackToTop.innerHTML = '<a class="btn-floating btn-small btn-flat waves-effect waves-light nav-back-to-top tooltipped" data-tooltip="Back to top"><i class="material-icons">arrow_upward</i></a>';
     navIndexHeading.appendChild(navIndexHeadingContent);
     navIndexHeading.appendChild(navIndexBackToTop);
     tocHeader.appendChild(navIndexHeading);
-    document.querySelector('.nav-back-to-top').addEventListener('click', function(e){
-      document.querySelector('#slide-out').scrollTo(0,0);
+    document.querySelector('.nav-back-to-top').addEventListener('click', function (e) {
+      document.querySelector('#slide-out').scrollTo(0, 0);
     });
   }
-  
+
   var items = [];
-  document.querySelectorAll('.index').forEach(function(el){
-    items.push('<li><a href="#'+el.id+'"'+' class="waves-effect sidenav-close">'+el.id+'</a></li>');
+  document.querySelectorAll('.index').forEach(function (el) {
+    items.push('<li><a href="#' + el.id + '"' + ' class="waves-effect sidenav-close">' + el.id + '</a></li>');
   });
 
   var navUl = document.querySelector('#scrollspy-nav ul');
-  if(navUl){
+  if (navUl) {
     navUl.innerHTML = items.join('');
     navUl.classList.remove('hidden');
   }
 
-  var scrollSpyEnter = function(navElemSelector, id){
+  var scrollSpyEnter = function (navElemSelector, id) {
     var scrollspyNavElem = document.querySelector('#scrollspy-nav');
     var navElem = scrollspyNavElem.querySelector(navElemSelector);
-    scrollspyNavElem.querySelectorAll('.active').forEach(function(el){
+    scrollspyNavElem.querySelectorAll('.active').forEach(function (el) {
       el.classList.remove('active');
     })
-    if(navElem){
+    if (navElem) {
       navElem.classList.add('active');
       document.querySelector('#slide-out').scrollTo(window.scrollX, navElem.offsetTop - scrollspyNavElem.offsetTop)
     }
@@ -53,10 +53,25 @@ document.addEventListener('DOMContentLoaded', function() {
   var debounceScrollSpyEnter = debounce(scrollSpyEnter, 100);
   M.ScrollSpy.init(document.querySelectorAll('.index'), {
     scrollOffset: SCROLL_OFFSET,
-    getActiveElement: function(id){
+    getActiveElement: function (id) {
       var navElemSelector = 'a[href="#' + id + '"]';
       debounceScrollSpyEnter(navElemSelector, id);
       return navElemSelector;
+    }
+  });
+
+  // odang! try to create hyperlinkz for "see also"
+  var seeAlsoClick = function (e) {
+    if (e.target && e.target.innerText !== '') {
+      var searchSeeAlso = document.evaluate("//strong[contains(., '" + e.target.innerText.trim() + "')]", document, null, XPathResult.ANY_TYPE, null);
+      var seeAlso = searchSeeAlso.iterateNext();
+      seeAlso && window.scrollTo(0, seeAlso.offsetTop + SCROLL_OFFSET);
+    }
+  }
+  document.querySelectorAll('.see-also').forEach(function (el) {
+    if (document.evaluate("//strong[contains(., '" + el.innerText.trim() + "')]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength) {
+      el.classList.add('anchor');
+      el.addEventListener('click', seeAlsoClick);
     }
   });
 
